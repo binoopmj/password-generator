22ccec21392cee2324a4a3ab055096e2588f31f1
Binoop MJ <binoop.mj@fayausa.com>
2024-01-26 22:04:27 +0530

    feat(theme): strength function and css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Clipboard } from '@angular/cdk/clipboard';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,7 +12,10 @@ export class AppComponent {
     includeSymbols: boolean = false;
     passwordLength: number = 12;
     generatedPassword: string = '';
-  
+    strength: string= ''; 
+
+    
+    constructor(private clipboard: Clipboard) {}
     generatePassword() {
       const numbers = '0123456789';
       const uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -38,5 +41,47 @@ export class AppComponent {
       }
   
       this.generatedPassword = password;
+      this.strength = this.getStrength();
+      
     }
+    getStrength() {
+      if (this.includeNumbers && this.includeUppercase && this.includeLowercase && this.includeSymbols && this.passwordLength > 8) {
+        return 'strong';
+      }
+      if (this.includeNumbers && this.includeUppercase && this.includeLowercase && this.includeSymbols) {
+        return 'medium';
+      }
+      if (this.includeNumbers && this.includeUppercase && this.includeLowercase && this.includeSymbols && this.passwordLength < 4) {
+        return 'weak';
+      }
+      else if (this.includeNumbers && this.includeUppercase ) {
+        return 'medium';
+      }
+      else if (this.includeNumbers && this.includeLowercase ) {
+        return 'medium';
+      }
+      else if (this.includeNumbers && this.includeSymbols) {
+        return 'medium';
+      }
+      else if (this.passwordLength > 8 ){
+        return 'medium';
+      }
+      else if (this.passwordLength < 8 ){
+        return 'veryweak';
+      }
+      else if (this.passwordLength < 8 && this.includeNumbers || this.includeUppercase || this.includeLowercase || this.includeSymbols){
+        return 'medium';
+      }
+      else {
+        return 'veryweak';
+      }
+    }
+    copyToClipboard() {
+      if(this.generatedPassword){
+        this.clipboard.copy(this.generatedPassword);
+        alert('Copied to clipboard');
+      }
+      
+    }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ClipboardModule } from '@angular/cdk/clipboard';
 
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    ClipboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
